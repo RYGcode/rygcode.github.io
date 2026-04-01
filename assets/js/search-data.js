da7626596e1444d18da82d2aeafb0e53b0406df2
RYGcode <94727970+RYGcode@users.noreply.github.com>
2026-04-01 10:57:24 +0000

    Deploying to gh-pages from @ RYGcode/rygcode.github.io@846149a698116c5f27bdeedceeb04c651e8caa55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -110,14 +110,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_ReinaldoYosafatGultom.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%65%69%6E%61%64%6F%67%75%6C%74%6F%6D@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -125,13 +125,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
