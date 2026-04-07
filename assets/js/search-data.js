967592c5e210a5016cd4e6ce42ab1d2d3ed6d8cd
RYGcode <94727970+RYGcode@users.noreply.github.com>
2026-04-07 17:41:56 +0000

    Deploying to gh-pages from @ RYGcode/rygcode.github.io@9a66925830818a84533f9f550c87a0fe10110b65 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,9 +65,9 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-amc-in-wireless-communication-using-cnn",
+          title: 'AMC in wireless communication using CNN',
+          description: "my academic project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
