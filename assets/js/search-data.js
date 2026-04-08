cbb380eedc329cdf89a2ef83b652ccbe602d70ea
RYGcode <94727970+RYGcode@users.noreply.github.com>
2026-04-08 06:45:44 +0000

    Deploying to gh-pages from @ RYGcode/rygcode.github.io@52cf25880b6d38e0a39af4bb685555e0697a70d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,9 +70,9 @@ ninja.data = [{
           description: "my academic project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-indonesian-abusive-tweet-classification-based-on-cnns-and-lstms",
+          title: 'Indonesian Abusive Tweet Classification based on CNNs and LSTMs',
+          description: "publishing project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
